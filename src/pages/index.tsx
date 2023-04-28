341c657c0d9d8f77f9fa9f2abf2117fd547b2f26
Thirstums <janis27burch@gmail.com>
2023-04-28 22:42:37 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,8 +35,7 @@ export default function Home() {
               <input type="text" id='UserInputforms' />
               <button type='submit'>Submit</button>
             </form>
-            
-            
+
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
