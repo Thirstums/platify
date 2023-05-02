e5082b4c9b5117ddd1ad6a69c64dc0cf6fccae00
Thirstums <Janis27burch@gmail.com>
2023-05-02 15:04:45 +0200

    Frontend update

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+// components/Navbar.tsx
+import React from "react";
+import Link from "next/link";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const Footer = () => {
+  return (
+    <>
+    </>
+    
+  );
+};
+export default Footer;
