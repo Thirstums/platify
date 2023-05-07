0026fdfc1c1013d618d985e3e0b35cd2d0168dbb
alessiioo <alessio2005fiorante@gmail.com>
2023-05-07 16:39:23 +0200

    add git-crypt

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as process from "process";
 
 type Data = {
-  name: string
+  name: string | undefined
 }
 
+let abcc=process.env.hello
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: abcc})
 }
