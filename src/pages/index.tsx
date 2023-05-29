7279fce3f19764b0d5110aa4ad9ef8d79f05a3b0
Moiz Malik <mma126188@iet-gibb.ch>
2023-05-29 23:42:18 +0200

    add tag functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,53 +5,55 @@ import styles from '@/styles/Home.module.scss'
 import { createPlaylistByMatchingSongs, spotifyApi } from './api/spotify'
 import { getTrackList } from './api/openai'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getToken, refreshAccessToken } from './api/auth/spotify-auth'
+import React from "react"; 
+import { TagsInput } from "react-tag-input-component"; 
 
 const inter = Inter({ subsets: ['latin'] })
 const TOKEN_REFRESH_INTERVAL = 55 * 60 * 1000; // refresh the token every 55 minutes
 
 export default function Home() {
+  const [selected, setSelected] = useState<string[]>(["gfg"]); // State for selected tags
+
   // Handles the submit event on form submit.
   const handleSubmit = async (event: any) => {
-  // Stop the form from submitting and refreshing the page.
-  event.preventDefault()
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
 
-  // Get data from the form.
-  const data = {
-    UserInputforms: event.target.UserInputforms.value
-    
-  }
+    // Get data from the form.
+    const data = {
+      UserInputforms: event.target.UserInputforms.value
+    }
 
-  // Send the data to the server in JSON format.
-  const JSONdata = JSON.stringify(data)
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
 
-  // API endpoint where we send form data.
-  const endpoint = '/api/form'
+    // API endpoint where we send form data.
+    const endpoint = '/api/form'
 
-  // Form the request for sending data to the server.
-  const options = {
-    // The method is POST because we are sending data.
-    method: 'POST',
-    // Tell the server we're sending JSON.
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // Body of the request is the JSON data we created above.
-    body: JSONdata,
-  }
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
 
-  // Send the form data to our forms API on Vercel and get a response.
-  const response = await fetch(endpoint, options)
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
 
-  // Get the response data from server as JSON.
-  // If server returns the name submitted, that means the form works.
-  const result = await response.json()
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
 
-
-  getTrackList(event.target.UserInputforms.value).then(res =>
-    createPlaylistByMatchingSongs(res)
-  );
+    getTrackList(event.target.UserInputforms.value).then(res =>
+      createPlaylistByMatchingSongs(res)
+    );
   }
 
   const router = useRouter();
@@ -93,37 +95,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        {/*<div className={styles.center}>*/}
-        {/*  <div className={styles.description}>*/}
         <form onSubmit={handleSubmit} className={styles.form}>
           <label htmlFor="UserInputforms">A<br/>Playlist<br/>Generator</label>
           <div className={styles.inputParent}>
-            <input type="text" id='UserInputforms' name='UserInputforms' placeholder={"Add a Tag. For example: \"90s music\"" }/>
-            <button type = 'button' className={styles.addTagsbtn}>Add Tags</button>
+            <TagInput selected={selected} setSelected={setSelected} /> {/* Render the TagInput component */}
+            <button type="button" className={styles.addTagsbtn}>Add Tags</button>
           </div>          
           <div className={styles.generatebtn}>
-          <button type='submit'>Generate Playlist</button>
+            <button type="submit">Generate Playlist</button>
           </div>
         </form>
 
-        {/*</div>*/}
-        {/*</div>*/}
-        {/*<div className={styles.grid}>*/}
         <a
-            href="https://github.com/Thirstums/platify"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {/*<h2 className={inter.className}>*/}
-            {/*  Contact Us <span>-&gt;</span>*/}
-            {/*</h2>*/}
-            {/*<p className={inter.className}>*/}
-            {/*  help.platify@gmail.com*/}
-            {/*</p>*/}
-
-            <Image
-
+          href="https://github.com/Thirstums/platify"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
             src="/github-mark-white.png"
             alt="GitHub Logo"
             width={30}
@@ -131,30 +120,21 @@ export default function Home() {
             className={styles.githubImage}
           />
         </a>
-        {/*  <a*/}
-        {/*    href="https://github.com/Thirstums/platify"*/}
-        {/*    className={styles.card}*/}
-        {/*    target="_blank"*/}
-        {/*    rel="noopener noreferrer"*/}
-        {/*  >*/}
-        {/*    <h2 className={inter.className}>*/}
-        {/*      Github <span>-&gt;</span>*/}
-        {/*    </h2>*/}
-        {/*    <p className={inter.className}>*/}
-        {/*      Platify Official Github*/}
-        {/*    </p>*/}
-
-        {/*    <Image*/}
-
-        {/*    src="/GithubLogo.png"*/}
-        {/*    alt="GitHub Logo"*/}
-        {/*    width={90}*/}
-        {/*    height={35}*/}
-        {/*    priority*/}
-        {/*  />*/}
-        {/*  </a>*/}
-        {/*</div>*/}
       </main>
     </>
-  )
+  );
+}
+
+function TagInput({ selected, setSelected }: { selected: string[], setSelected: React.Dispatch<React.SetStateAction<string[]>> }) {
+  return (
+    <>
+      <pre>{JSON.stringify(selected)}</pre>
+      <TagsInput
+        value={selected}
+        onChange={setSelected}
+        placeHolder="Add a Tag. For example: '90s music'"
+        name='UserInputforms'
+      />
+    </>
+  );
 }
